1fd5705cdd480ecb680de290a0708e2ed94b95eb
karunru_labo <1qazhikaru+lab@gmail.com>
2015-12-16 13:39:50 +0900

    imreadしてimshowできた

--- first_test.py
+++ first_test.py
@@ -4,22 +4,23 @@ import matplotlib.pyplot as plt
 
 argvs = sys.argv
 if (len(argvs) != 2):
-	print 'Usage: $ python %s filename' % argvs[0]
-quit()
+	print('Usage: $ python %s filename' % argvs[0])
+	exit()
  
 imagefilename = argvs[1]
 try:
 	img = cv2.imread(imagefilename, 1)
 except:
-	print 'faild to load %s' % imagefilename
-	quit()
+	print('faild to load %s' % imagefilename)
+	exit()
  
 # view image
 windowName = 'LoadImage'
 cv2.imshow( windowName, img)
-print 'press any key to exit.'
- 
 cv2.waitKey(0)
+print('press any key to exit.')
+cv2.destroyAllWindows()
+
 plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
